b0e31e6877ed1e2ce964897a037d32a45b29f54c
HyunA-Kim <39957484+HyunA-Kim@users.noreply.github.com>
2019-04-25 23:48:13 +0900

    케빈베이컨의 6단계법칙(2차커밋)
    - Algorithm : BFS, DFS, 플로이드 와샬 알고리즘
    - Progress
       > map 생성 및 각 맵의 연결된 선 표시작업완료
       > DFS를 이용해 각각의 점에서 목표지점까지의 거리 표시 함수 정의 완료
       > isVisited의 깊이를 이용해 목표지점의 거리를 이전방문노드의 거리에 더해주는 형식으로 수정
       > 각각의 방문횟수를 더하여 총 방문횟수를 구함
       > 가장 작은 방문횟수를 가지고 있는 사람의  숫자를 출력
    - Problem
       > 해당 프로그램을 실행 시 동작이 정상적으로 진행됨
       > 예외적인 상황이 있을 수 있으므로 체크 필요
       > 플로이드 와샬 알고리즘 숙지 필요

--- [1389] 케빈베이컨의 6단계법칙.cpp
+++ [1389] 케빈베이컨의 6단계법칙.cpp
@@ -5,12 +5,10 @@
 using namespace std;
 
 int N, M;
-int min_count=1000;
+int min_count=100000;	//최소 방문횟수
 int man_num=0;
 
 vector<vector<int>> map;
-vector<vector<int>> map_visited;
-vector<int> man;
 
 void DFS(int A) {
 	
@@ -18,29 +16,26 @@ void DFS(int A) {
 	stack<int> s;
 
 	isVisited.assign(N, 0);
-	s.push(A);
-	int count = 0;
+	s.push(A);	//해당 좌표를 stack에 입력
 
 	while (!s.empty()) {
 		int k = s.top();
 		s.pop();
-
-			for(int i=0; i<N; i++){
-				if (map[k][i] != 0 && map_visited[k][i] == 0) {
-					isVisited[i] = count;
-					s.push(i);
-					map_visited[k][i] = 1;
-					map_visited[i][k] = 1;
-				}
+		for (int i = 0; i < N; i++) {
+			if (i == A) continue;
+			if (map[k][i] == 1 && isVisited[i]== 0 ) {
+				isVisited[i] = isVisited[k] + 1;	//깊이를 계산하기 위해 전 방문노드의 수에서 더함
+				s.push(i);	
 			}
-		count++;
+		}
 	}
 	
 	int cnt = 0;
 	for (int i = 0; i < N; i++) {
-		cnt += isVisited[i];
+		cnt += isVisited[i];	//방문횟수 더하기
 	}
 
+	//서로간의 방문횟수 비교 및 저장
 	if (min_count > cnt) {
 		man_num = A;
 		min_count = cnt;
@@ -55,7 +50,6 @@ int main(void) {
 	cin >> N >> M;
 
 	map.assign(N, vector<int>(N, 0));
-	man.assign(N, 0);
 
 	for (int i = 0; i < M; i++) {
 		cin >> A >> B;
@@ -64,9 +58,9 @@ int main(void) {
 	}
 
 	for (int i = 0; i < N; i++) {
-		map_visited.assign(N, vector<int>(N, 0));
-		DFS(i);
+		DFS(i);	//각각 방문횟수를 카운트하기 위한 함수
 	}
 
-	cout << man_num+1;
+	cout << man_num+1;	//가장 작은 방문횟수 가지고 있는 사람 출력
+	return 0;
 }
