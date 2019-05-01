49178a815fb908c10305fdaf1cc86f1085be503f
HyunA-Kim <39957484+HyunA-Kim@users.noreply.github.com>
2019-05-01 23:38:10 +0900

    이분 그래프(2차커밋)
    - Algorithm : DFS, BFS
    - Process
       > map 생성 및 map_visited 그래프를 점의 갯수만큼만 구현
       > 해당 그래프를 DFS를 이용해 연결되어있는지 확인 
          (재귀구문으로 돌아갈 때마다 -1,1 반복을 통해 이분그래프인지를 판별)
       > 해당 그래프가 이분그래프인지 확인하기 위해 판별 함수를 선언 및 정의
    - Problem
       > 이분그래프인지 판별하기 위해 isVisited 부분을 0이아닌 1과 -1로 판별할 수 있도록 함
       > 예외처리 필요

--- [1707] 이분 그래프.cpp
+++ [1707] 이분 그래프.cpp
@@ -7,25 +7,31 @@ using namespace std;
 int V, E;
 
 vector<vector<int>> map;
-vector<vector<int>> map_visited;
+vector<int> isVisited;
 
-void DFS(int num) {
-	
-	stack<int> s;
-	vector<int> isVisited;
-
-	s.push(num);
-	isVisited.assign(V, 0);
-
-	while (!s.empty()) {
-		int k = s.top();
-		s.pop();
-		for (int i = 0; i < V; i++) {
-			if (map[k][i] == 1 && map_visited[k][i] != 0) {
-				map_visited[k][i] *= -1;
+void isBinary() {
+	for (int i = 0; i < V; i++) {
+		int find = isVisited[i];
+		for (int j = 0; j < map[i].size(); j++) {
+			if (find == isVisited[map[i][j]]) {
+				cout << "NO" << endl;
+				return;
 			}
 		}
 	}
+	cout << "YES" << endl;
+	return;
+}
+
+void DFS(int num, int find) {
+
+	for (int i = 0; i < map[num].size(); i++) {
+		if (!isVisited[map[num][i]]) {
+			isVisited[map[num][i]] = find * -1;
+			DFS(map[num][i], find*-1);
+		}
+	}
+
 }
 
 int main(void) {
@@ -36,20 +42,22 @@ int main(void) {
 	while (T--) {
 		cin >> V >> E;
 		map.assign(V, vector<int>(V, 0));
-		map_visited.assign(V, vector<int>(V, 0));
+		isVisited.assign(V, 0);
 
 		for (int i = 0; i < E; i++) {
 			int x, y;
 			cin >> x >> y;
 			map[x - 1][y - 1] = 1;
 			map[y - 1][x - 1] = 1;
-			map_visited[x - 1][y - 1] = 1;
-			map_visited[y - 1][x - 1] = 1;
+		
 		}
 
 		for (int i = 0; i < V; i++) {
-			DFS(i);
+			isVisited[i] = 1;
+			DFS(i, 1);
 		}
+
+		isBinary();
 	}
 
 }
