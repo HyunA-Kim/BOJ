f873bfcc846de23b1171f206a3b43f81d8038c51
HyunA-Kim <39957484+HyunA-Kim@users.noreply.github.com>
2019-05-03 22:12:18 +0900

    이분 그래프(완성)
    - Algorithm : DFS, BFS
    - Point : DFS BFS 알고리즘 숙지, 이분그래프의 이해 및 학습, 이분그래프를 어떻게 파악하고 나타낼 것인지 충분한 생각이 필요함
    - My Alogrithm : DFS
       > 순서 : 
           1. vector map 자체를 정점들의 나열로 정의 -> 각연결된 선 추가시에 해당정점에 정점번호를 추가
           2. 1부터 V까지의 정점을 차례로 이분그래프 판별위해 DFS실행
           3. DFS 실행시 해당 정점 방문시 각각을 1과 -1을 번갈아가면서 isVisited에 기입 ( 이분그래프 판별위해)
           4. 해당 정점의 isVisited와 같은 입력값일 경우, 이분그래프가 아님을 출력. 모든 결과 아닐시 이분그래프임을 출력
       > 이분그래프의 판별을 어떻게 처리할 것인지를 인지하는 것이 중요 (isVisited-> 1,-1를 이용해 이분그래프를 판별하도록 한다)
       > 간선을 연결했다는 처리시, push_back을 이용해 해당 연결 정점자체를 추가하는 방법도 있다는 것을 알아둘 것.

--- [1707] 이분 그래프.cpp
+++ [1707] 이분 그래프.cpp
@@ -1,24 +1,32 @@
 #include<iostream>
 #include<vector>
-#include<stack>
 
 using namespace std;
 
 int V, E;
 
-vector<vector<int>> map;
+vector<int> map[20001];
 vector<int> isVisited;
 
+//해당 그래프가 이분그래프인지 판별
 void isBinary() {
-	for (int i = 0; i < V; i++) {
+
+	for (int i = 1; i <= V; i++) {
+	
 		int find = isVisited[i];
+		
 		for (int j = 0; j < map[i].size(); j++) {
+			
+			//해당그래프의 isVisited이 find와 동일할시, 같은부분 -> 이분그래프가 아님
 			if (find == isVisited[map[i][j]]) {
 				cout << "NO" << endl;
 				return;
 			}
+
 		}
 	}
+
+	//동일한 부분이 없을시, 홀수사이클이 아니므로 이분그래프
 	cout << "YES" << endl;
 	return;
 }
@@ -26,8 +34,14 @@ void isBinary() {
 void DFS(int num, int find) {
 
 	for (int i = 0; i < map[num].size(); i++) {
+
+		//해당그래프 정점을 방문하지 않았을 때
 		if (!isVisited[map[num][i]]) {
+
+			//가지고 있던 변수와 다른 변수를 저장해줌
 			isVisited[map[num][i]] = find * -1;
+			
+			//또다른 연결선 찾는 재귀함수(recursive function start)
 			DFS(map[num][i], find*-1);
 		}
 	}
@@ -41,23 +55,32 @@ int main(void) {
 
 	while (T--) {
 		cin >> V >> E;
-		map.assign(V, vector<int>(V, 0));
-		isVisited.assign(V, 0);
+		isVisited.assign(V+1, 0);
 
 		for (int i = 0; i < E; i++) {
+
 			int x, y;
 			cin >> x >> y;
-			map[x - 1][y - 1] = 1;
-			map[y - 1][x - 1] = 1;
+			//맵에 해당 정점 추가
+			map[x].push_back(y);
+			map[y].push_back(x);
 		
 		}
 
-		for (int i = 0; i < V; i++) {
-			isVisited[i] = 1;
-			DFS(i, 1);
+		for (int i = 1; i <= V; i++) {
+
+			//각 정점마다 연결되어있는 간선찾기(find connected line)
+			if (!isVisited[i]) {
+				isVisited[i] = 1;
+				DFS(i, 1);
+			}
 		}
 
+		//이분그래프인지 판별함수 ( Discrimination Bipartite Graph)
 		isBinary();
+
+		//맵 리셋(map reset)
+		for (int i = 1; i <= V; i++) map[i].clear();
 	}
 
 }
