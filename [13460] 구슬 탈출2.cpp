4d806d89b0e5171550a5bac28149ef1fe53f7315
HyunA-Kim <39957484+HyunA-Kim@users.noreply.github.com>
2019-05-10 23:31:11 +0900

    구슬 탈출2(완성)
    - Algorithm : BFS, 브루트-포스(Bruth-Force)
    - Point : 구슬을 상하좌우 방향으로 굴릴 때, count를 어떻게 해줄 것인지에 대한 처리와 겹쳤을 때, 어떻게 처리할 것인지가 중요
    - My Algorithm : BFS
       > 순서 
          1.  map에 구슬과, 빈공간, 구멍들을 기입
          2. 'R', 'B', 'O'의 좌표들을 pair<int,int> 를 사용해서 따로 기입하며, queue를 실행시킬 수 있도록함
          3. 한뱡향이 최선이 아닐 수 있으므로, queue의 사이즈만큼 반복할 수 있도록 처리
          4. 상하좌우라는 것에 맞춰 각 방향에 따라 구슬을 움직일 수 있도록 하며 이때 예외처리를 제대로해주어야함
              ※  예외처리 : 구슬이 겹칠시, 구슬이 map 영역바깥으로 나갈시, 'B'구슬이 '0'공간에 들어갈때, count가 10이상일때
         5. 각 가정에 따라 완료하였을 경우, count를 증가시키며 다시 queue에 있는 것으로 실행시킬 수 있도록 함.
       > 예외처리 상황을 제대로 계산 후 구현하는 것이 굉장히 중요하다
       > vector<vector<char>>보다 배열을 사용하는 것이 메모리효율이 더 좋다. (메모리가 덜 잡아먹음)
       > pair<int,int> a, pair<int,int> b같은 경우 a==b와 같은 방식으로 처리할 수 있다는 것을 알게되었다.

--- [13460] 구슬 탈출2.cpp
+++ [13460] 구슬 탈출2.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<queue>
-
+#include<algorithm>
 using namespace std;
 
 int N, M;
@@ -9,63 +9,65 @@ int ans = 0;
 int dir[4][2] = { {-1,0},{1,0},{0,-1},{0,1} };
 
 vector<vector<char>> map;
+int isVisited[11][11][11][11];
 
-vector<vector<int>> rq_visited;
-vector<vector<int>> bq_visited;
-
-queue<pair<int,int>> rq;
+queue<pair<int, int>> rq;	
 queue<pair<int, int>> bq;
 
-pair<int, int> goal;
-pair<int, int> r;
-pair<int, int> b;
+pair<int, int> goal;	//'0'의 좌표
+pair<int, int> r;		//'R'의 좌표
+pair<int, int> b;		//'B'의 좌표
 
 void BFS() {
+
 	while (rq.size()) {
+	
 		int size = rq.size();
+		
 		while (size--) {
-			r.first = rq.front().first;
+
+			r.first = rq.front().first;		//각 queue 의 가장앞 원소들을 꺼낸 후, 제거
 			r.second = rq.front().second;
+			
 			b.first = bq.front().first;
 			b.second = bq.front().second;
+		
 			rq.pop();
 			bq.pop();
 
-			if (r == goal && r == b) {
-				cout << -1;
-				return;
-			}
-			else if (r == goal && r != b) {
-				if (ans >= 10) {
-					cout << -1;
-				}
-				else {
-					cout << ans;
-				}
+			// R구슬이 골인했을 경우, 출력 ('B'는 골인하면 -1이므로 안됨)
+			if (r == goal && r != b) {
+				cout << ans;
 				return;
 			}
 
+			// 각 방향대로 구슬을 굴리기 (상,하,좌,우)
 			for (int k = 0; k < 4; k++) {
 
 				pair<int, int> new_r = { r.first,r.second };
 				pair<int, int> new_b = { b.first,b.second };
 
+				// 'R'구슬을 k방향으로 굴리기(move R with k direction)
 				while (1) {
 					if (map[new_r.first + dir[k][0]][new_r.second + dir[k][1]] == '#') break;
-					if (new_r.first == goal.first && new_r.second == goal.second) break;
+					if (new_r == goal) break;
 					new_r.first += dir[k][0];
 					new_r.second += dir[k][1];
 				}
 
+				// 'B'구슬을 k방향으로 굴리기(move B with k direction)
 				while (1) {
 					if (map[new_b.first + dir[k][0]][new_b.second + dir[k][1]] == '#') break;
-					if (new_b.first == goal.first && new_b.second == goal.second) break;
+					if (new_b == goal) break;
 					new_b.first += dir[k][0];
 					new_b.second += dir[k][1];
 				}
 
+				// 'R'과 'B'좌표가 같을 경우(구멍에 들어간 경우 제외하고는 같을 수 없다는 조건문에 의해)
+
 				if (new_r == new_b && new_r != goal) {
-					//열이다른경우
+
+					// 열이다른경우
 					if (r.first == b.first) {
 						if (r.second > b.second) {
 							if (k == 2) new_r.second += 1;
@@ -76,57 +78,56 @@ void BFS() {
 							if (k == 3) new_r.second -= 1;
 						}
 					}
-					//행이 다른경우
+	
+					// 행이 다른경우
 					else if (r.second == b.second) {
 						if (r.first > b.first) {
-							if (k == 0) new_r.second += 1;
-							if (k == 1) new_b.second -= 1;
+							if (k == 0) new_r.first += 1;
+							if (k == 1) new_b.first -= 1;
 						}
 						else {
-							if (k == 0) new_b.second += 1;
-							if (k == 1) new_r.second -= 1;
+							if (k == 0) new_b.first += 1;
+							if (k == 1) new_r.first -= 1;
 						}
 					}
+			
 				}
 
-				if (new_r == goal || new_b == goal) continue;
-				if (bq_visited[new_b.first][new_b.second] == 1 && rq_visited[new_r.first][new_r.second] == 1) continue;
+				// 이미 방문한 경우 스킵(already visited don't count)
+				if (isVisited[new_r.first][new_r.second][new_b.first][new_b.second] == 1) continue;
 
-				rq_visited[new_r.first][new_r.second] = 1;
-				bq_visited[new_b.first][new_b.second] = 1;
+				isVisited[new_r.first][new_r.second][new_b.first][new_b.second] = 1;
 
-				map[r.first][r.second] = '.';
-				map[b.first][b.second] = '.';
-				map[new_r.first][new_r.second] = 'R';
-				map[new_b.first][new_b.second] = 'B';
-
-
+				// queue에 집어넣기
 				rq.push(make_pair(new_r.first, new_r.second));
 				bq.push(make_pair(new_b.first, new_b.second));
-
-				for (int i = 0; i < N; i++) {
-					for (int j = 0; j < M; j++) {
-						cout << map[i][j] << " ";
-					}
-					cout << endl;
-				}
 			}
-			ans++;
 		}
+
+		// 10번이상일 움직였을 경우 실패(more than 10 counts, failed)
+		if (ans == 10) {
+			cout << -1;
+			return;
+		}
+		// count 1증가
+		ans++;
 	}
+
+	cout << -1;
+	return;
 }
 
 int main(void) {
-	
-	cin >> N >> M;
-	map.assign(N, vector<char>(M, '.'));
 
-	rq_visited.assign(N, vector<int>(M, 0));
-	bq_visited.assign(N, vector<int>(M, 0));
+	cin >> N >> M;
+
+	map.assign(N, vector<char>(M, '.'));
 
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
+
 			cin >> map[i][j];
+			
 			if (map[i][j] == 'R') {
 				r.first = i;
 				r.second = j;
@@ -144,6 +145,9 @@ int main(void) {
 
 	rq.push(make_pair(r.first, r.second));
 	bq.push(make_pair(b.first, b.second));
-
+	
+	isVisited[r.first][r.second][b.first][b.second] = 1;
+	
 	BFS();
+
 }
