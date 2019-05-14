b52f9ef322b0c12fc202887cc89c811748941c50
HyunA-Kim <39957484+HyunA-Kim@users.noreply.github.com>
2019-05-14 23:19:50 +0900

    보물섬(2차커밋)
    - Algorithm : BFS
    - Process
       > 맵 생성 및 관련 맵 정의
       > BFS를 이용해 맵에서 땅을 갈만한 곳을 탐색  -> 이 때, 각 depth를 비교하여 max보다 크다면 업데이트
       > 각 맵의 땅에서 실행할 수 있도록함(좌표마다 실행)
    - Problem
       > 각 육지마다 셀수 있는 것으로 바꿈 -> 최대한 가장 먼곳에서 실행되어야 하므로
       > 각 maximum을 어떻게 처리해줄 것인지 조건을 따져볼 것.

--- [2589] 보물섬.cpp
+++ [2589] 보물섬.cpp
@@ -6,47 +6,13 @@ using namespace std;
 
 vector<vector<char>> map;
 vector<vector<int>> map_visited;
-vector<vector<int>> isLand_visited;
+vector<vector<int>> depth;
 
 queue<pair<int, int>> q;
-queue<pair<int, int>> q2;
-queue<pair<int, int>> q3;
 
 int M, N;
 int dir[4][2] = { {-1,0},{1,0},{0,-1},{0,1} };
-int minimum = 10000;
-
-void findMaximum() {
-
-	isLand_visited.assign(M, vector<int>(N, 0));
-	int ans = 0;
-	while (q3.size()) {
-		int size = q3.size();
-		while (size--) {
-			
-			int y = q3.front().first;
-			int x = q3.front().second;
-			q3.pop();
-
-			for (int i = 0; i < 4; i++) {
-				
-				int new_y = y + dir[i][0];
-				int new_x = x + dir[i][1];
-				
-				if (new_y < 0 || new_y >= M || new_x < 0 || new_x >= N) continue;
-
-				if (map[new_y][new_x] == 'L' && isLand_visited[new_y][new_x] == 1) {
-					isLand_visited[new_y][new_x] = 1;
-					q3.push(make_pair(new_y, new_x));
-				}
-			}
-			ans++;
-
-		}
-	}
-
-	minimum = min(minimum, ans);
-}
+int maximum = 0;
 
 bool map_q() {
 	for (int i = 0; i < M; i++) {
@@ -54,7 +20,6 @@ bool map_q() {
 			if (map[i][j] == 'L' && map_visited[i][j] == 0) {
 				map_visited[i][j] = 1;
 				q.push(make_pair(i, j));
-				q2.push(make_pair(i, j));
 				return true;
 			}
 		}
@@ -78,18 +43,13 @@ void BFS() {
 				if (map[new_y][new_x] == 'L' && map_visited[new_y][new_x] == 0) {
 					map_visited[new_y][new_x] = 1;
 					q.push(make_pair(new_y, new_x));
-					q2.push(make_pair(new_y, new_x));
+
+					depth[new_y][new_x] = depth[new_y][new_x] + 1;
+					if (maximum < depth[new_y][new_x])
+						maximum = depth[new_y][new_x];
 				}
 			}
-		}
-		int size = q2.size();
-		while(size--){
-			int y = q2.front().first;
-			int x = q2.front().second;
-			q2.pop();
-			q3.push(make_pair(y, x));
-			findMaximum();
-		}
+		}		
 	}
 }
 
@@ -106,7 +66,16 @@ int main(void) {
 		}
 	}
 
-	BFS();
+	for (int i = 0; i < M; i++) {
+		for (int j = 0; j < N; j++) {
+			if (map[i][j] == 'L') {
+				map_visited.assign(M, vector<int>(N, 0));
+				depth.assign(M, vector<int>(N, 0));
+				q.push(make_pair(i, j));
+				BFS();
+			}
+		}
+	}
 
-	cout << minimum;
+	cout << maximum;
 }
