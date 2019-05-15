7cb5d8c7faa11d96bcdce17edd3bfe89bda7a49d
HyunA-Kim <39957484+HyunA-Kim@users.noreply.github.com>
2019-05-15 17:19:41 +0900

    보물섬(완성)
    - Algorithm : BFS
    - Point : 최대길이를 구하기 위해서는, 모든 점에서 최대길이를 실행해봐야한다는 점이 중요. 각 점마다 비교필요
    - My Algorithm : BFS
       > 순서
          1. 맵 생성 및 정보 기록
          2. 각 지점에서 가장 먼곳을 알아보려면 이어져있는 'L'을 계산필요
          3. 연결되어있는 'L' 계산 위해 BFS 함수 정의
          4. 각 함수에서 섬에 가는 최대길이를 작성 후 max값 출력
       > 각 지점마다 첫번째 지점으로 두고, 가장 먼곳땅까지 가는 길이를 출력해야함

--- [2589] 보물섬.cpp
+++ [2589] 보물섬.cpp
@@ -14,43 +14,33 @@ int M, N;
 int dir[4][2] = { {-1,0},{1,0},{0,-1},{0,1} };
 int maximum = 0;
 
-bool map_q() {
-	for (int i = 0; i < M; i++) {
-		for (int j = 0; j < N; j++) {
-			if (map[i][j] == 'L' && map_visited[i][j] == 0) {
-				map_visited[i][j] = 1;
-				q.push(make_pair(i, j));
-				return true;
+void BFS() {
+
+	while (!q.empty()) {
+		
+		int y = q.front().first;
+		int x = q.front().second;
+		q.pop();
+
+		for (int i = 0; i < 4; i++) {
+			int new_y = y + dir[i][0];
+			int new_x = x + dir[i][1];
+
+			if (new_y < 0 || new_y >= M || new_x < 0 || new_x >= N) continue;
+
+			if (map[new_y][new_x] == 'L' && map_visited[new_y][new_x] == 0) {
+
+				map_visited[new_y][new_x] = 1;
+				q.push(make_pair(new_y, new_x));
+
+				depth[new_y][new_x] = depth[y][x] + 1;	// 총 길이 계산 = 가장멀리떨어져있는 'L'찾기
+				
+				if (maximum < depth[new_y][new_x])
+					maximum = depth[new_y][new_x];	//가장 먼 곳일 때 업데이트
 			}
 		}
 	}
-	return false;
-}
 
-void BFS() {
-	while (1) {
-		if (map_q() == false) return;
-		while (!q.empty()) {
-			int y = q.front().first;
-			int x = q.front().second;
-			q.pop();
-			for (int i = 0; i < 4; i++) {
-				int new_y = y + dir[i][0];
-				int new_x = x + dir[i][1];
-				
-				if (new_y < 0 || new_y >= M || new_x < 0 || new_x >= N) continue;
-
-				if (map[new_y][new_x] == 'L' && map_visited[new_y][new_x] == 0) {
-					map_visited[new_y][new_x] = 1;
-					q.push(make_pair(new_y, new_x));
-
-					depth[new_y][new_x] = depth[new_y][new_x] + 1;
-					if (maximum < depth[new_y][new_x])
-						maximum = depth[new_y][new_x];
-				}
-			}
-		}		
-	}
 }
 
 int main(void) {
@@ -68,14 +58,21 @@ int main(void) {
 
 	for (int i = 0; i < M; i++) {
 		for (int j = 0; j < N; j++) {
+
+			//각각 지점마다 먼곳이 다르므로, for문을 이용하여 각 지점마다 먼곳을 찾아준다.
 			if (map[i][j] == 'L') {
-				map_visited.assign(M, vector<int>(N, 0));
+
+				map_visited.assign(M, vector<int>(N, 0));	//다른 지점 방문을 위한 초기화
 				depth.assign(M, vector<int>(N, 0));
-				q.push(make_pair(i, j));
-				BFS();
+				
+				q.push(make_pair(i, j));	//해당 지점부터 방문하고 싶은경우 queue에다 저장
+				map_visited[i][j] = 1;
+
+				BFS();	//BFS실행
 			}
+
 		}
 	}
 
-	cout << maximum;
+	cout << maximum;	//가장 먼곳값을 임력
 }
