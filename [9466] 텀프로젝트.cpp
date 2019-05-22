38865febb507d1f82dd88b01332c3508fcae5a85
HyunA-Kim <39957484+HyunA-Kim@users.noreply.github.com>
2019-05-22 23:46:36 +0900

    텀프로젝트(완성)
    - Algorithm : DFS, 사이클 찾기
    - Point : 해당 사이클 계산시, 이미 카운트가 된 것들은 중복계산하지 않도록 처리하는 것이 중요. 
    - My Algorithm : DFS
       > 순서
          1. 해당 맵을 완성 및 맵 구조설계
          2. 맵에서 사이클을 찾기 위해 해당 맵의 값을 DFS를 이용해 들어갈 것.
          3. 이미 방문한 맵을 방문시 사이클이 생기는 조건이므로, 카운트에 들어감
          4. 모든 맵 인덱스에 대해 실행 후, 전체 맵 사이즈에서 해당 카운트 값을 빼주면 사이클 없는 곳이 출력
       > 맵을 완성시, 중간에 자기자신을 가르키는 노드가 존재할 경우 그에 관련된 모든 노드는 카운트를 취소해주어야함.
       > 맵의 사이클이 중복으로 세지지 않도록 만드는 것이 중요

--- [9466] 텀프로젝트.cpp
+++ [9466] 텀프로젝트.cpp
@@ -1,54 +1,33 @@
 #include<iostream>
 #include<vector>
-#include<stack>
 
 using namespace std;
 int N;
+int ans = 0;
 
 vector<int> map;
-vector<int> map_visited;
-
-stack<int> s;
+vector<int> map_visited;	//맵의 방문여부
+vector<int> map_finished;	//해당 숫자의 카운트가 끝났는지 확인
 
 void DFS(int i,int goal) {
 
-	//if (map[i] == i) {
-	//	while (!s.empty()) {
-	//		int k = s.top();
-	//		s.pop();
-	//		map_visited[k] = 0;
-	//	}
-	//	map_visited[i] = 1;
-	//	return;
-	//}
-
-	if (map_visited[i] == 1 ) {
-		if (s.empty()) {
-			map_visited[i] = 1;
-		}
-		else {
-			int k = s.top();
-			s.pop();
-			while (!s.empty()) {
-				int z = s.top();
-				s.pop();
-				if (k == z) break;
-			}
-
-			while (!s.empty()) {
-				int z = s.top();
-				s.pop();
-				map_visited[z] = 0;
-			}
-		}
-		return;
-
-	}	
-	map_visited[i] = 1;
+	map_visited[i] = 1;	//맵을 방문했다는 확인
 	int k = map[i];
-	s.push(i);
-	DFS(k,goal);
 
+	//해당 맵의 결과값으로 DFS실행
+	if (map_visited[k] == 0) {
+		DFS(k, goal);
+	}
+	//해당 맵의 결과값이 이미 방문했으며, 카운트가 끝나지 않았을 경우
+	//카운트 실행
+	else if(map_finished[k]==0){
+		for (int j = k; j != i; j = map[j])
+			ans++;
+		ans++;
+		//카운트
+	}
+
+	map_finished[i] = 1;	//해당 인덱스 방문끝
 }
 
 int main(void) {
@@ -57,25 +36,23 @@ int main(void) {
 	cin >> T;
 
 	while (T--) {
+		ans = 0;
 		cin >> N;
 
 		map.assign(N+1, 0);
 		map_visited.assign(N + 1, 0);
-		
+		map_finished.assign(N + 1, 0);
+
 		for (int i = 1; i <= N; i++) {
 			cin >> map[i];
 		}
 
 		for (int i = 1; i <= N; i++) {
-			DFS(i, i);
+			if(map_visited[i]==0) 
+				DFS(i, i);
 		}
-
-		int cnt = 0;
 		
-		for (int i = 1; i <= N; i++) {
-			if (map_visited[i] == 0) cnt++;
-		}
-
-		cout << cnt << endl;
+		ans = N - ans;	//방문안한 것으로 업데이트
+		cout << ans << endl;
 	}
 }
