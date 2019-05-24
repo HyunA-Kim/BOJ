933b1735c3b6544ebceb7d6a9691741101976b82
HyunA-Kim <39957484+HyunA-Kim@users.noreply.github.com>
2019-05-24 23:11:01 +0900

    스타트링크(완성)
    - Algorithm : BFS
    - Point : 층 수를 벗어나거나 혹은 U==0 or D==0 인 같은 예외상황 처리를 해주는 것이 포인트, 그 이외에 최소방문수를 구하기 위한 작업 필요
    - My Algorithm : BFS
       > 순서
          1. 맵 생성 및 주어진 변수에 맞게 맵 크기 설정
          2. 해당 맵에서 층 수가 벗어날 경우 계단을 사용할 수 있도록 예외처리함
          3. BFS를 이용해 U사용할경우, D 사용할 경우로 나뉘어 각각 최소방문수를 전에서 1을 더한 것을 저장
          4. 해당 지점에 도착했을 시 방문수를 출력
       > 런타임에러 발생이유는 line 23,31에서 else if대신 if를 사용했기 때문
       > Except_case() 함수를 사용하지 않아도 해당 queue에서 더이상 저장되는 수가 없으면 모두 예외처리사항으로 구분하여 출력됨 
          = 코드 간편화가능
       > BFS를 이용해 각 경우마다 goal를 찾을 수 있도록 만들어놓는 설계가 중요

--- [5014] 스타트링크.cpp
+++ [5014] 스타트링크.cpp
@@ -5,70 +5,92 @@
 using namespace std;
 
 int F, S, G, U, D;
-int dir[2];
-vector<int> map;
-queue<int> q;
+int dir[2];	//Up,Down 기록
 
+vector<int> map;	
+queue<int> q;	
+
+//예외조건 명시
 bool Except_Case() {
+	
 	bool finish = false;
+
+	//더이상 올라가거나 내려갈 경우가 없을 경우 계단이용(use the stairs, when can't use elevator)
 	if (S < G) {
 		if (U == 0) {
-			cout << "use the stairs";
 			finish = true;
 		}
-		if (D == 0 && (G - S) % U != 0) {
-			cout << "use the stairs";
+		else if (D == 0 && (G - S) % U != 0) {
 			finish = true;
 		}
 	}
 	else if (S > G) {
 		if (D == 0) {
-			cout << "use the stairs";
 			finish = true;
 		}
-		if (U == 0 && (S - G) % D != 0) {
-			cout << "use the stairs";
+		else if (U == 0 && (S - G) % D != 0) {
 			finish = true;
 		}
-	}
-	else if (S == G) {
-		cout << map[S - 1];
-		finish = true;
 	}
 
+	if (finish == true) 
+		cout << "use the stairs";
+	
 	return finish;
 }
 void BFS() {
 
-	if (Except_Case() == true) return;
+	//예외조건에서 걸러질 경우 리턴(return when catch the except_case)
+	if (S == G) {
+		cout << map[S - 1];
+		return;
+	}
+	else if (Except_Case() == true) return;
 
+	//맵에 현재 위치 저장(save the map with current location)
 	map[S - 1] = 1;
 	q.push(S - 1);
 
 	while (!q.empty()) {
+
 		int k = q.front();
 		q.pop();
+
+		//도착 했을 경우(when arrived at goal)
+		if (k == G - 1) {
+			cout << map[k] - 1;
+			return;
+		}
+
+		//위로 올라가거나, 내려가는 동작(move to up and down case)
 		for (int i = 0; i < 2; i++) {
+
 			int new_k = k + dir[i];
-			if (new_k < 0 || new_k >= F) continue;
+			
+			if (new_k < 0 || new_k >= F) continue;	//배열을 벗어낫을 경우, 동작이 일어나지 않도록 함(if out of index, do not anything)
+			
 			if (map[new_k] == 0) {
-				map[new_k] = map[k] + 1;
-				q.push(new_k);
+				map[new_k] = map[k] + 1;	//최소 움직임의 수를 기록(note count of minimum move)
+				q.push(new_k);	//다음 칸으로 갈 수 있도록 queue에 저장(save in queue to move next)
 			}
-			if (new_k == G - 1) {
-				cout << map[new_k]-1;
-				return;
-			}
+
 		}
 	}
+
+	//해당 queue에서 끝나지 않았을 경우, 계단이용(use the stairs, when can't go to the goal)
+	cout << "use the stairs";
+	return;
 }
 
 int main(void) {
+
 	cin >> F >> S >> G >> U >> D;
 
+	//Up,Down 기록(note the up and down)
 	dir[0] = U;
 	dir[1] = -D;
-	map.assign(F, 0);
+
+	map.assign(F+1, 0);
 		
-	BFS();
+	BFS();	//BFS실행(action BFS)
 }
