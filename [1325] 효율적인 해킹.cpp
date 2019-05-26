9c922895c0fc618896e72caffaf5feecfecc95ff
HyunA-Kim <39957484+HyunA-Kim@users.noreply.github.com>
2019-05-26 23:43:24 +0900

    효율적인 해킹(완성)
    - Algorithm : DFS
    - Point : 해킹하기 위해서는 각 컴퓨터에서 해킹이 가능한지 순차적으로 실행해 갯수를 카운팅해야함
    - My Algorithm : DFS
       > 순서
          1. map생성 및 각 map에서 해킹 가능한 컴퓨터 번호를 입력
          2. DFS를 실행하여 각 컴퓨터에서 더 들어가 해킹이 가능한지 확인
          3. 이때, 각 해킹 한 컴퓨터갯수를 카운트
          4. 해킹 컴퓨터 카운팅을 마쳤을 경우, 다음 컴퓨터 카운팅때 사용하기 위해 초기화
       > 컴퓨터 카운팅을 위해선 각 변수를 초기화해주는 것이 중요
       > DFS사용시 해킹수를 더해주며 세줘야함

--- [1325] 효율적인 해킹.cpp
+++ [1325] 효율적인 해킹.cpp
@@ -1,38 +1,55 @@
 #include<iostream>
 #include<vector>
-#include<stack>
 
 using namespace std;
 
 int N, M;
-vector<vector<int>> map;
-vector<vector<int>> map_visited;
+int ans = 0;
+int maximum = 0;
 
-stack<int> s;
+vector<int> map[10001];
+vector<int> map_visited;
+vector<int> computer;	//컴퓨터를 해킹할 수 있는 수 (count on computer hacking)
 
-void DFS(int pos,int cnt) {
-	while (!s.empty()) {
-		int k = s.top();
-		s.pop();
-		if (map_visited[k] == 0) {
-			for (int i = 0; i < map[k].size(); i++) {
-				s.push(map[k][i]);
-			}
+void DFS(int pos) {
+
+	map_visited[pos] = 1;
+	
+	for (int i = 0; i < map[pos].size(); i++) {
+	
+		if (map_visited[map[pos][i]] == 0) {
+			ans++;	//해킹한 수 더하기 (count hacking computer)
+			DFS(map[pos][i]);	//더 해킹할 수 있는지 확인(check possible hacking computer)
 		}
+
 	}
 }
 
 int main(void) {
 	
 	cin >> N >> M;
-	map.assign(N + 1, vector<int>(N+1,0));
-	map_visited.assign(N + 1, vector<int>(N + 1, 0));
+	computer.assign(N + 1, 0);
+
 	while (M--) {
 		int x, y;
 		cin >> x >> y;
 		map[y].push_back(x);
-		for (int i = 0; i < N; i++) {
-			DFS(i,0);
+
+	}
+
+	for (int i = 1; i <= N; i++) {
+		ans = 0;	//각 컴퓨터에서 실행하므로 초기화(initalize because each computer do)
+		map_visited.assign(N + 1, 0);	//각 컴퓨터에서 실행하므로 초기화(initalize because each computer do)
+		DFS(i);
+		computer[i] = ans;	//해킹 한 수 저장(save hacking computers)
+		if (maximum < ans) maximum = ans;	//가장 큰 수 확인(check maximum number)
+	}
+
+
+	for (int i = 1; i <= N; i++) {
+		if (computer[i] == maximum) {
+			cout << i << " ";	//가장 큰 수 가지고 있는 컴퓨터 출력(print if there has a lot of maximum computer)
 		}
 	}
+
 }
