0f159795d4ecd7409c972f3fee92d10082cf92d6
HyunA-Kim <39957484+HyunA-Kim@users.noreply.github.com>
2019-06-04 23:36:02 +0900

    탈출(완성)
    - Algorithm : BFS, 시뮬레이션
    - Point : 비버가 다음칸으로 이동할 때, 미리 물의 다음 영역구하기 ( 알고리즘 순서를 바꾸면 해결이 가능) , queue를 사용할 때 메모리를 많이 차지한다는 점을 알고 있도록 해야함.
    - My Algorithm : BFS
       > 순서
          1. 해당 맵에서 중요한 세개의 변수(비버, 동굴, 물) 위치를 확보 및 저장
          2. 비버와 물은 동시에 상하좌우로 퍼지므로 해당 queue를 이용해 상하좌우로 확장할 수 있도록 함
          3. 비버는 이동경로의 수를 출력하기 위해 map_visited를 갈 때마다 1씩 올려줌
          4. 비버가 동굴에 도착했을 시 그 때의 이동경로횟수를 출력, 도착이 불가능경우 fail메시지 출력
       > 비버가 물이 올 다음영역은 피해야 하므로 알고리즘 순서를 바꿔주도록 한다. ( 물확장 -> 비버확장순 : 미리 물이 다음영역으로 들어감)
    
       > 메모리초과 문제 해결 : queue를 사용할 경우, 불필요한 queue의 확장은 메모리를 초과하는 문제를 만들게 된다. 최대한 간편하게 작성하기 위해 배열 사용을 고려할 것
       > 런타임에러 문제 해결 : 런타임에러는 배열에서 접근하지 말아야할 곳을 접근할 시 많이 발생된다. 해당 범위를 제대로 살펴보려는 노력이 중요
       > map_visited를 char형으로 두는 초보적인 실수를 하여 문제 해결이 늦어졌다.  처음 문제를 해결시, 필요한 자료형과 어떻게 쓰일지를 잘 보고 판단하며 기록할 수 있도록 하는 실력을 기르는 것이 필요하다.

--- [3055] 탈출.cpp
+++ [3055] 탈출.cpp
@@ -8,83 +8,95 @@ int dir[4][2] = { {-1,0},{1,0},{0,-1},{0,1} };
 int R, C;
 
 vector<vector<char>> map;
-vector<vector<char>> map_visited;
+vector<vector<int>> map_visited;
 
 pair<int, int> man;
 pair<int, int> cave;
 queue<pair<int, int>> water;
-queue<pair<pair<int, int>,int>> biber;
+queue<pair<int, int>> biber;
 
 void BFS() {
 	
-	bool expect_waterbound = false;
+	while (!biber.empty()) {
+		
+		int man_size = biber.size();	//biber의 사이즈만큼 회전하기 위해 변수를 저장(save size of the biber to spread around the area)
+		int water_size = water.size();	//water의 사이즈만큼 회전하기 위해 변수를 저장(save size of the water to spread around the area)
 
-	while (1) {
+		//물이 먼저 와야 biber에 물 처리조건을 제외한 간편한 알고리즘 가능
+		for (int q = 0; q < water_size; q++) {
 
-		if (biber.empty()) break;
-		pair<int, int> man_move = biber.front().first;
-		int cnt = biber.front().second;
-		biber.pop();
-		pair<int, int> water_move = water.front();
-		water.pop();
-	
-		for (int i = 0; i < 4; i++) {
-			pair<int, int> new_water_move = { water_move.first + dir[i][0], water_move.second + dir[i][1] };
-			if (new_water_move.first < 0 || new_water_move.first >= R || new_water_move.second < 0 || new_water_move.second >= C) continue;
-			if (map[new_water_move.first][new_water_move.second] == '.') {
-				water.push(new_water_move);
-				map[new_water_move.first][new_water_move.second] = '*';
-			}
-		}
+			pair<int, int> water_move = water.front();
+			water.pop();
 
+			//상하좌우로 퍼트리기(spread around the area)
+			for (int i = 0; i < 4; i++) {
 
-		for (int i = 0; i < 4; i++) {
-
-			pair<int, int> new_man_move = { man_move.first + dir[i][0],man_move.second + dir[i][1] };
-			pair<int, int> new_water_move = { water.front().first,water.front().second };
-			if (new_man_move.first < 0 || new_man_move.first >= R || new_man_move.second < 0 || new_man_move.second >= C) continue;
-			//물이 들어올 예정인칸 처리할 수 있는 로직 구현
-			expect_waterbound = false;
-			for (int z = 0; z < 4; z++) {
-				if ((new_man_move.first == new_water_move.first + dir[z][0]) && (new_man_move.second == new_water_move.second + dir[z][1]))
-				{
-					expect_waterbound = true;
-					break;
+				pair<int, int> new_water_move = { water_move.first + dir[i][0], water_move.second + dir[i][1] };
+				if (new_water_move.first < 0 || new_water_move.first >= R || new_water_move.second < 0 || new_water_move.second >= C) continue;
+				
+				//빈 공간일 경우, 물이 퍼짐 (if empty space, water spread)
+				if (map[new_water_move.first][new_water_move.second] == '.') {
+					water.push(new_water_move);	//다음 물 공간 확보
+					map[new_water_move.first][new_water_move.second] = '*';
 				}
-			}
-			if (map[new_man_move.first][new_man_move.second] == '.'&& expect_waterbound == false) {
-				biber.push({ new_man_move,cnt + 1 });
-				map_visited[new_man_move.first][new_man_move.second] = 1;
-			}
-			if (cave == new_man_move) {
-				cout << cnt + 1;
-				return;
+
 			}
 		}
 
-		
-		
+		//biber가 갈 수 있는 방향을 정하기
+		for (int q = 0; q < man_size; q++) {
+
+			pair<int, int> man_move = biber.front();
+			biber.pop();
+
+			//상하좌우로의 움직임(move all position to go to cave)
+			for (int i = 0; i < 4; i++) {
+
+				pair<int, int> new_man_move = { man_move.first + dir[i][0],man_move.second + dir[i][1] };
+				if (new_man_move.first < 0 || new_man_move.first >= R || new_man_move.second < 0 || new_man_move.second >= C) continue;
+			
+				//다음 영역이 cave일 경우, 도착(arrive when position is cave)
+				if (new_man_move == cave) {
+					cout << map_visited[man_move.first][man_move.second]+1;	//얼만큼 걸렸는지를 출력
+					return;
+				}
+
+				//빈 공간일 경우(벽이나 물이 없는경우) 비버 보내기
+				if (map[new_man_move.first][new_man_move.second] == '.' && map_visited[new_man_move.first][new_man_move.second]==0) {
+					biber.push(new_man_move);
+					map_visited[new_man_move.first][new_man_move.second] = map_visited[man_move.first][man_move.second] + 1;	//한칸 진전을 뜻함
+				}
+				
+			}
+		}
 	}
 
+	//도착하지 못하였을 경우 fail 메시지를 출력
 	cout << "KAKTUS";
 	return;
 }
+
 int main(void) {
 
 	cin >> R >> C;
 	map.assign(R+1, vector<char>(C+1, 0));
-	map_visited.assign(R+1, vector<char>(C+1, 0));
+	map_visited.assign(R+1, vector<int>(C+1, 0));
 
 	for (int i = 0; i < R; i++) {
 		for (int j = 0; j < C; j++) {
+			
 			cin >> map[i][j];
+			
+			//biber의 위치일경우 biber초기위치를 man에 기입
 			if (map[i][j] == 'S') {
 				man = { i,j };
-				biber.push({ man,0 });
+				biber.push(man);
 			}
+			//동굴위치 기입
 			else if (map[i][j] == 'D') {
 				cave = { i,j };
 			}
+			//물위치 기입
 			else if (map[i][j] == '*') {
 				water.push({ i,j });
 			}
