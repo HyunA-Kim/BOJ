8970fb3571c0d5b309d45ac062b406bf79511852
HyunA-Kim <39957484+HyunA-Kim@users.noreply.github.com>
2019-06-08 23:47:25 +0900

    Puyo Puyo(4차커밋)
    - Algorithm : DFS, BFS, 시뮬레이션
    - Process
       > 해당 문제 이해 및 파악
       > 이해판단에 맞춰 필요한 알고리즘 선별
       > 해당 공의 색깔이 다 다르므로 공 색깔을 정해주도록 하고 연쇄적용이일어났을 경우 지워주도록 하는 로직 구현
       > 해당 연쇄작용을 일어나게하는 공을 삭제하는 로직 구현
      > 하나의 맵에 연쇄를 일으킬 수 있는 공이 여러개라면 한번에 시행할 수 있도록 만듬
    - Problem
       > 상쇄되서 없어졌을 경우 새로운 map을 탐색했을 때, return 값을 조심할 것
       > 로직의 input과 output을 통해 어떤 로직에 오류가 생겼는지에 대한 파악 필요

--- [11559] Puyo Puyo.cpp
+++ [11559] Puyo Puyo.cpp
@@ -11,6 +11,7 @@ vector<vector<char>> map;
 vector<vector<int>> map_visited;
 	
 queue<pair<int, int>> del;
+queue<pair<int, int>> save;
 int dir[4][2] = { {-1,0},{0,-1},{0,1},{1,0} };
 int ans = 0;
 //해당 뿌요를 삭제했을 경우 테트리스가 밑으로 내려옴
@@ -32,7 +33,6 @@ bool DFS(pair<int, int> org, int cnt) {
 	
 	if (cnt == 4) {
 		cnt = 0;
-		Delete();
 		return true;
 	}
 
@@ -44,9 +44,11 @@ bool DFS(pair<int, int> org, int cnt) {
 		if (new_y < 0 || new_y >= R || new_x < 0 || new_x >= C) continue;
 
 		if (map[new_y][new_x] == map[org.first][org.second] && map_visited[new_y][new_x]==0) {
-			del.push({ new_y,new_x });
+			save.push({ new_y,new_x });
 			map_visited[new_y][new_x] = 1;
-			DFS({ new_y,new_x },cnt+1);
+			if (DFS({ new_y,new_x }, cnt + 1) == true) {
+				return true;
+			}
 		}
 	}
 
@@ -57,7 +59,7 @@ bool map_checked() {
 	for (int i = 0; i < R; i++) {
 		for (int j = 0; j < C; j++) {
 			if (map[i][j] != '.' && map_visited[i][j]==0) {
-				del.push({ i,j });
+				save.push({ i,j });
 				map_visited[i][j] = 1;
 				return true;
 			}
@@ -76,24 +78,38 @@ int main(void) {
 			cin >> map[i][j];
 		}
 	}
-
-
+	
 	while (1) {
-		if (map_checked() == false) break;
-		if (DFS({ del.front().first, del.front().second }, 1) == true) {
-			map_visited.assign(R, vector<int>(C, 0));
-			ans++;
-		}
-		while (!del.empty()) {
-			del.pop();
-		}
 		for (int i = 0; i < R; i++) {
 			for (int j = 0; j < C; j++) {
-				cout << map[i][j] << " ";
+				if (map[i][j] != '.' && map_visited[i][j] == 0) {
+					save.push({ i,j });
+					map_visited[i][j] = 1;
+					if (DFS({ i,j }, 1) == true) {
+						while (!save.empty()) {
+							del.push(save.front());
+							save.pop();
+						}
+					}
+					else {
+						while (!save.empty()) {
+							save.pop();
+						}
+					}
+
+				}
 			}
-			cout << endl;
 		}
-		cout << endl;
+
+		map_visited.assign(R, vector<int>(C, 0));
+
+		if (!del.empty()) {
+			Delete();
+			ans++;
+		}
+		else {
+			break;
+		}
 	}
 
 	cout << ans;
