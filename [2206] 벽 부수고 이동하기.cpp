1ab5bb6f67f76f6d7c6e01fc07a90817471816c4
HyunA-Kim <39957484+HyunA-Kim@users.noreply.github.com>
2019-06-11 23:50:26 +0900

    벽 부수고 이동하기(1차커밋)
    - Algorithm : BFS
    - Process
       > 해당 문제 이해 및 파악
       > 이해판단에 맞춰 필요한 알고리즘 선별
       > 벽을 부술 수 있는 조건이 추가되었으므로 벽을 허물고, 다시 벽을 세우는 알고리즘을 선별
    - Problem
       > 벽이 허문 후, 다시 원상복귀 시키는 로직의 완성 필요
       > 해당 벽에 알맞은 조건일때, 앞으로 나아갈 수 있도록 하며 최소값 출력할 수 있도록 하는 것이 중요

---
+++ [2206] 벽 부수고 이동하기.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+
+using namespace std;
+
+int N, M;
+int crush = 0;
+int minimum = 100;
+
+int dir[4][2] = { {-1,0},{1,0},{0,-1},{0,1} };
+
+vector<vector<int>> map;
+vector<vector<int>> map_visited;
+
+queue<pair<int, int>> q;
+
+void BFS() {
+	int ans = 1;
+	q.push({ 0,0 });
+	
+	while (!q.empty()) {
+		int y = q.front().first;
+		int x = q.front().second;
+		q.pop();
+		for (int i = 0; i < 4; i++) {
+
+			int new_y = y + dir[i][0];
+			int new_x = x + dir[i][1];
+			if (new_y < 0 || new_y >= N || new_x < 0 || new_x >= M) continue;
+			if (map[new_y][new_x]== 0 && map_visited[new_y][new_x] == 0) {
+				map_visited[new_y][new_x] = 1;
+				q.push({ new_y,new_x });
+				ans++;
+			}
+
+		}
+	}
+
+	if (minimum > ans) minimum = ans;
+}
+int main(void) {
+	
+	cin >> N >> M;
+
+	map.assign(N, vector<int>(M, 0));
+	map_visited.assign(N, vector<int>(M, 0));
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			cin >> map[i][j];
+		}
+	}
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			map_visited.assign(N, vector<int>(M, 0));
+
+			if (map[i][j] == 1) {
+				crush++;
+				map[i][j] = 0;
+				BFS();
+				map[i][j] = 1;
+			}
+
+		}
+	}
+
+	cout << minimum;
+}
