b25870f5c39110b9908bac62336e798d7dd652df
HyunA-Kim <39957484+HyunA-Kim@users.noreply.github.com>
2019-06-14 23:35:37 +0900

    벽 부수고 이동하기(3차커밋)
    - Algorithm : BFS
    - Process
       > 해당 문제 이해 및 파악
       > 이해판단에 맞춰 필요한 알고리즘 선별
       > 벽을 부술 수 있는 조건이 추가되었으므로 벽을 허물고, 다시 벽을 세우는 알고리즘을 선별
       > 조건을 마지막 영역까지 도착했을 때, minimum값을 비교할 수 있도록 추가
       > 주어진 코드에서 시간초과문제를 해결하기 위한 알고리즘 재구성할 방법을 생각함
    - Problem
       > 주어진 코드는, BFS와 백트래킹을 이용한 방법으로 모든 경로를 한번씩 방문하며 가장 최소값을 출력함
       > 해당 코드를 이용하면, 주어진 결과값이 일치하지만 모든 경로의 방문으로 시간초과 발생
       > 시간초과를 해결하기 위해 모든 경로를 한번씩 방문하는 코드 대신 다른 로직 구현이 필요

--- [2206] 벽 부수고 이동하기.cpp
+++ [2206] 벽 부수고 이동하기.cpp
@@ -13,7 +13,7 @@ int dir[4][2] = { {-1,0},{1,0},{0,-1},{0,1} };
 vector<vector<int>> map;
 vector<vector<int>> map_visited;
 
-queue<pair<int, int>> q;
+queue<pair<pair<int, int>,int>> q;
 
 void BFS() {
 	int ans = 1;
@@ -24,8 +24,9 @@ void BFS() {
 		
 		isFind = false;
 
-		int y = q.front().first;
-		int x = q.front().second;
+		int y = q.front().first.first;
+		int x = q.front().first.second;
+		int wall = q.front().second;
 		q.pop();
 
 		for (int i = 0; i < 4; i++) {
@@ -33,12 +34,14 @@ void BFS() {
 			int new_y = y + dir[i][0];
 			int new_x = x + dir[i][1];
 			if (new_y < 0 || new_y >= N || new_x < 0 || new_x >= M) continue;
-			if (map[new_y][new_x]== 0 && map_visited[new_y][new_x] == 0) {
+			if (map[new_y][new_x] == 1 && wall == 0) {
+				q.push({ {new_y,new_x},1 });
+			}
+			else if (map[new_y][new_x] == 0 && map_visited[new_y][new_x] == 0) {
 				map_visited[new_y][new_x] = 1;
-				q.push({ new_y,new_x });
+				q.push({{ new_y,new_x }, 0});
 				isFind = true;
-				//시간초과를 위해 안에 집어넣음
-				if (new_y == N - 1 && new_x == M - 1 && q.front().first == new_y && q.front().second == new_x) {
+				if (new_y == N - 1 && new_x == M - 1 && q.front().first.first == new_y && q.front().first.second == new_x) {
 					if (minimum > ans)
 					{
 						minimum = ans;
@@ -62,7 +65,7 @@ int main(void) {
 
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
-			scanf("%1d", &map[i][j]);
+			scanf_s("%1d", &map[i][j],sizeof(map[i][j]));
 		}
 	}
 
@@ -70,7 +73,6 @@ int main(void) {
 		for (int j = 0; j < M; j++) {
 			map_visited.assign(N, vector<int>(M, 0));
 			if (map[i][j] == 1) {
-				//백트래킹 방법
 				map[i][j] = 0;
 				BFS();
 				map[i][j] = 1;
